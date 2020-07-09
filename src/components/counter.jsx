82a50747b65a28e07d896624da5e956cd762d8b9
JessieYG11 <yueguan1110@gmail.com>
2020-07-09 15:37:38 +0200

    Display a picture based on dynamic object property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,12 +7,13 @@ class Counter extends Component {
   //all instance variables are stored here
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200",
   };
 
   //render is a keyword that acts like a main
   //all methods are defined like this
   render() {
-    //if the value of count changes
+    //if the value of count or imageUrl changes
     //the page will automatically update
     //you can write any javascript expression
     //between the curly braces and tags
@@ -20,6 +21,7 @@ class Counter extends Component {
     //and display the result
     return (
       <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
