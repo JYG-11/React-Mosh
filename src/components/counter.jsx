1de07f040fb67e3803b34b29c1698b35cf0097ef
JessieYG11 <yueguan1110@gmail.com>
2020-07-09 15:10:53 +0200

    Display dynamic object property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  //state is a keyword that acts like a dict
+  //but in fact it is actually an object
+  //count is unique for each class instance
+  //all instance variables are stored here
+  state = {
+    count: 0,
+  };
+
+  //render is a keyword that acts like a main
+  //all methods are defined like this
   render() {
+    //if the value of count changes
+    //the page will automatically update
     return (
-      <React.Fragment>
-        <h1>Counter App</h1>
+      <div>
+        <h1>{this.state.count}</h1>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
   }
 }
