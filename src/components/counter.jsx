5351840e2271910e619e6d313131029e3a0baa4a
JessieYG11 <yueguan1110@gmail.com>
2020-07-09 14:48:32 +0200

    Alternative export syntax

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
+export default class Counter extends Component {
   state = {};
   render() {
     return <h1>Counter App</h1>;
   }
 }
-
-export default Counter;
