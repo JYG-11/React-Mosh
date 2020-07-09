a91bfac3ec42d00290a7032725956e5a57f917c8
JessieYG11 <yueguan1110@gmail.com>
2020-07-09 15:40:52 +0200

    Styling elements based on bootstrap classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,13 +7,12 @@ class Counter extends Component {
   //all instance variables are stored here
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
 
   //render is a keyword that acts like a main
   //all methods are defined like this
   render() {
-    //if the value of count or imageUrl changes
+    //if the value of count changes
     //the page will automatically update
     //you can write any javascript expression
     //between the curly braces and tags
@@ -21,16 +20,15 @@ class Counter extends Component {
     //and display the result
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-small">Increment</button>
       </div>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
