9e536a1e3393e82c928687a856be991a226a0d6f
JessieYG11 <yueguan1110@gmail.com>
2020-07-10 09:55:34 +0200

    Dynamically rendered list refactored

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,6 +10,18 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   //render is a keyword that acts like a main
   //all methods are defined like this
   render() {
@@ -20,28 +32,7 @@ class Counter extends Component {
     //you can even call on a function
     //and display the result
 
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-small">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return <div>{this.renderTags()}</div>;
   }
 }
 
